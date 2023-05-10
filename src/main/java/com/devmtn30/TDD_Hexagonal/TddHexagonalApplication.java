24f18b4ab896ca61eb22b7dc96976ec4a2f20254
devMtn30 <devMtn30@gmail.com>
2023-05-10 21:15:44 +0900

    project init

---
+++ src/main/java/com/devmtn30/TDD_Hexagonal/TddHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.devmtn30.TDD_Hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddHexagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devmtn30/TDD_Hexagonal/TddHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmtn30.TDD_Hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
