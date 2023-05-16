ed6df7c6458290311e5c212ee1904b05fd12a5cd
devMtn30 <devMtn30@gmail.com>
2023-05-16 21:27:20 +0900

    feat: RestAssured dependency 추가 및 상품등록 TDD Api Test로 전환 리펙토링

--- src/main/java/com/devmtn30/TDD_Hexagonal/ProductService.java
+++ src/main/java/com/devmtn30/TDD_Hexagonal/ProductService.java
@@ -1,8 +1,16 @@
 package com.devmtn30.TDD_Hexagonal;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+@RestController
+@RequestMapping("/products")
 class ProductService {
     private final ProductPort productPort;
 
@@ -10,9 +18,14 @@ class ProductService {
         this.productPort = productPort;
     }
 
-    public void addProduct(AddProductRequest request) {
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<Void> addProduct(@RequestBody final AddProductRequest request) {
         final Product product = new Product(request.name(), request.price(), request.discountPilicy());
 
         productPort.save(product);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
