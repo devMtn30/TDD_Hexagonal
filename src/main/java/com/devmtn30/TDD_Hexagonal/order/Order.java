70d8a97c094e9a9fa7b4bfe874eb8094246e2022
devMtn30 <devMtn30@gmail.com>
2023-05-19 22:03:16 +0900

    refactor: 상품주문 JPA로 전환

--- src/main/java/com/devmtn30/TDD_Hexagonal/order/Order.java
+++ src/main/java/com/devmtn30/TDD_Hexagonal/order/Order.java
@@ -1,25 +1,32 @@
 package com.devmtn30.TDD_Hexagonal.order;
 
 import com.devmtn30.TDD_Hexagonal.Product;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.util.Assert;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "orders")
+@NoArgsConstructor
+@Getter
 class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private final Product product;
-    private final int quantity;
+    @OneToOne
+    private Product product;
+    private int quantity;
+
+    public Product getProduct() {
+        return product;
+    }
 
     public Order(Product product, int quantity) {
         this.product = product;
         this.quantity = quantity;
         Assert.notNull(product, "상품은 필수입니다.");
         Assert.isTrue(quantity > 0, "수량은 0보다 커야 합니다.");
-    }
-
-    public void assignId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
     }
 }
