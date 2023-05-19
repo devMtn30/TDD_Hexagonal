d7da6adb4621c7a30587da8bd6e5b51fb896bf24
devMtn30 <devMtn30@gmail.com>
2023-05-19 21:29:40 +0900

    feat: TDD 상품주문기능 POJO 개발

---
+++ src/test/java/com/devmtn30/TDD_Hexagonal/order/OrderServiceTest.java
@@ -0,0 +1,124 @@
+package com.devmtn30.TDD_Hexagonal.order;
+
+import com.devmtn30.TDD_Hexagonal.DiscountPilicy;
+import com.devmtn30.TDD_Hexagonal.Product;
+import com.devmtn30.TDD_Hexagonal.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class OrderServiceTest {
+
+    private OrderService orderService;
+    private OrderPort orderPort;
+
+    @BeforeEach
+    void setUp() {
+
+        OrderRepository orderRepository = new OrderRepository();
+        orderPort = new OrderPort() {
+            @Override
+            public Product getProductById(Long productId) {
+                return new Product("상품명", 1000, DiscountPilicy.NONE);
+            }
+
+            @Override
+            public void save(Order order) {
+                orderRepository.save(order);
+            }
+        };
+        orderService = new OrderService(orderPort);
+    }
+
+    @Test
+    void 상품주문() {
+        final Long productId = 1L;
+        final int quantity = 2;
+        final CreateOrderRequest request = new CreateOrderRequest(productId, quantity);
+        orderService.createOrder(request);
+    }
+
+    private record CreateOrderRequest(Long productId, int quantity) {
+        private CreateOrderRequest {
+            Assert.notNull(productId, "상품 ID는 필수입니다.");
+            Assert.isTrue(quantity > 0, "수량은 0보다 커야 합니다.");
+        }
+    }
+
+    private class OrderService {
+        private OrderPort orderPort;
+
+        public OrderService(OrderPort orderPort) {
+            this.orderPort = orderPort;
+        }
+
+        public void createOrder(CreateOrderRequest request) {
+            final Product product = orderPort.getProductById(request.productId());
+
+            final Order order = new Order(product, request.quantity());
+            orderPort.save(order);
+        }
+    }
+
+    private class OrderAdapter implements OrderPort {
+        private final ProductRepository productRepository;
+        private final OrderRepository orderRepository;
+
+        private OrderAdapter(ProductRepository productRepository, OrderRepository orderRepository) {
+            this.productRepository = productRepository;
+            this.orderRepository = orderRepository;
+        }
+
+        @Override
+        public Product getProductById(Long productId) {
+            return productRepository.findById(productId)
+                    .orElseThrow(() -> new IllegalArgumentException("상품이 존재하지 않습니다."));
+        }
+
+        @Override
+        public void save(Order order) {
+            orderRepository.save(order);
+        }
+
+    }
+
+    public interface OrderPort {
+        Product getProductById(Long productId);
+
+        void save(Order order);
+    }
+
+    private class Order {
+        private Long id;
+        private final Product product;
+        private final int quantity;
+
+        public Order(Product product, int quantity) {
+            this.product = product;
+            this.quantity = quantity;
+            Assert.notNull(product, "상품은 필수입니다.");
+            Assert.isTrue(quantity > 0, "수량은 0보다 커야 합니다.");
+        }
+
+        public void assignId(Long id) {
+            this.id = id;
+        }
+
+        public Long getId() {
+            return id;
+        }
+    }
+
+    private class OrderRepository {
+        private Map<Long, Order> persistence = new HashMap<>();
+        private Long sequence = 0L;
+
+        public void save(Order order) {
+            order.assignId(++sequence);
+            persistence.put(order.getId(), order);
+        }
+    }
+}
